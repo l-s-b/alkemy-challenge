25153b20e209aac2fbe935a79ad04860044be3cd
l-s-b <lucio_sb@yahoo.com.ar>
2021-10-26 00:14:17 -0300

    GET balance route: Funds update solved!

--- back/routes/1.get.js
+++ back/routes/1.get.js
@@ -5,6 +5,7 @@ const { Balance, Transaction } = require('../db');
 // GET BALANCE
 router.get('/main', async (_req, res, next) => {
     try {
+        let inflow = 0; let outflow = 0;
         const balance = await Balance.findAll({
             include: [{
                 model: Transaction,
@@ -12,6 +13,24 @@ router.get('/main', async (_req, res, next) => {
                 attributes: ["type", "amount", "item", "category"]
             },],
     });
+    // Update funds:
+    balance[0].dataValues.transactions.map( t => 
+    {
+        console.log("T TYPE: ", t.type);
+        console.log("T AMOUNT: ", t.amount);
+        console.log("TYPEOF T AMOUNT: ", typeof t.amount);
+        if(t.type === "INFLOW") { inflow += t.amount }
+        else if(t.type === "OUTFLOW") { outflow += t.amount }
+        ;} );
+    console.log("INFLOW: ", inflow);
+    console.log("TYPEOF INFLOW: ", typeof inflow);
+    console.log("TYPEOF INFLOW: ", typeof inflow);
+    console.log("OUTFLOW: ", outflow);
+    console.log("TYPEOF OUTFLOW: ", typeof outflow);
+    await Balance.update(
+        { funds: Math.round((inflow - outflow) * 100)/100 },
+        { where: { id: 1 } }
+    );
         return res.json(balance[0]);
     } catch(e) { next(e) }
 });
