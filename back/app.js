fd506516dab63dfaa9f640cfd60209fdf2c55402
lucio_sb@yahoo.com.ar <lucio_sb@yahoo.com.ar>
2021-10-26 14:26:05 -0300

    error catcher

--- back/app.js
+++ back/app.js
@@ -7,4 +7,12 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api', routes);
 
+// Error catching endware.
+app.use((err, _req, res, _next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+     console.error(err);
+    res.status(status).send(message);
+  });
+
 module.exports = app;
