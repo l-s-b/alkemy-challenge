d9711b23c26b260970bef991f2c654ae76d66f8a
lucio_sb@yahoo.com.ar <lucio_sb@yahoo.com.ar>
2021-10-27 09:26:20 -0300

    setting up routing and redux store in App

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './css/App.css';
+import './css/App.css'; // eslint-disable-next-line
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Bankemy</h2>
+      <Switch>
+{/*         <Route exacth path='/' component={} /> */}
+      </Switch>
     </div>
   );
 }
--- front/src/index.js
+++ front/src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
