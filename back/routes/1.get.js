6fee0a906839b6e2d200b74d825c3927770e5bce
lucio_sb@yahoo.com.ar <lucio_sb@yahoo.com.ar>
2021-10-30 00:56:26 -0300

    Category and Type queries in GET route

--- back/routes/1.get.js
+++ back/routes/1.get.js
@@ -35,9 +35,14 @@ router.get('/main', async (_req, res, next) => {
 });
 
 // GET ALL TRANSACTIONS
-router.get('/transactions', function(_req, res, next) {
+router.get('/transactions', function(req, res, next) {
+    const type = req.query.type;
+    const category = req.query.category;
     Transaction.findAll()
-        .then(results => res.json(results))
+        .then(results => !type ? results : results.filter(t => t.type === type)) // Type filter
+        .then(filtered1 => !category ? res.json(filtered1) :
+            res.json(filtered1.filter(t => t.category === category)) // Category filter
+            )
         .catch(err => next(err));
 });
 
